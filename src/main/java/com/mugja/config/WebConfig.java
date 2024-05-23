1bfc6686b004c272f239f060449eaf99e1d1a14c
sunniesfish <hhch5611.jh@gmail.com>
2024-05-23 17:33:51 +0900

    이미지 업로드 구현완료

--- src/main/java/com/mugja/config/WebConfig.java
+++ src/main/java/com/mugja/config/WebConfig.java
@@ -1,2 +1,17 @@
-package com.mugja.config;public class WebConfig {
+package com.mugja.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
 }
