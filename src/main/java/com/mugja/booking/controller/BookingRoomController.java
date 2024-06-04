75a89b76b10003398d1b02eba511cfa01ddcd3f7
Jamie Yoo <141890278+Jamie-Nestor90@users.noreply.github.com>
2024-06-04 16:07:42 +0900

    예약-결제 연동작업 진행중

--- src/main/java/com/mugja/booking/controller/BookingRoomController.java
+++ src/main/java/com/mugja/booking/controller/BookingRoomController.java
@@ -1,5 +1,8 @@
 package com.mugja.booking.controller;
 
+import com.mugja.member.service.SecurityService;
+import com.mugja.room.service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +11,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BookingRoomController {
-    
+
+    @Autowired
+    private SecurityService securityService;
+
+    @Autowired
+    private RoomService roomService;
+
     @GetMapping("/roomlist")
     public String showRoomList(Model model) {
         // 필요한 데이터를 모델에 추가
@@ -27,6 +36,7 @@ public class BookingRoomController {
         model.addAttribute("hostId", hostId);
         model.addAttribute("roomId", roomId);
         model.addAttribute("payPrice", payPrice);
+        model.addAttribute("userid", securityService.userId()); // 결제기능 합치기
         return "view/booking";
     }
 }
