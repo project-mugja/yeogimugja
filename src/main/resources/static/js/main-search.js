a7acb82564f93cf55d2640d15c5b0e3d14567d80
sunniesfish <hhch5611.jh@gmail.com>
2024-06-05 03:15:59 +0900

    검색 수정

--- src/main/resources/static/js/main-search.js
+++ src/main/resources/static/js/main-search.js
@@ -4,5 +4,9 @@ searchBtn? console.log("searchBtn") : console.log("no searchBtn");
 searchBtn.addEventListener("click",(event)=>{
     event.preventDefault();
     console.log("do search")
-    window.location.assign(`https://main--mugja.netlify.app/search/all/${searchInput.value}/${localStorage.getItem("token")}`);
+    let token = localStorage.getItem("token")? localStorage.getItem("token") : "";
+    let value = searchInput.value;
+    if(value){
+        window.location.assign(`https://main--mugja.netlify.app/search/all/${searchInput.value}/${token}`);
+    }
 })
