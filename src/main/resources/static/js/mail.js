f4a129c78fa1f1a174c2bf1382adba310f78b740
sunniesfish <hhch5611.jh@gmail.com>
2024-06-05 14:06:16 +0900

    일단ㅋㅋㅓ밋

--- src/main/resources/static/js/mail.js
+++ src/main/resources/static/js/mail.js
@@ -12,10 +12,17 @@ $(document).ready(function() {
 });
 
 function disableEmailField() {
-            document.getElementById("mem_email").disabled = true;
-            document.getElementById("email_auth").disabled = true;
-        }
-        
+    // Get the email value
+    var emailValue = document.getElementById("mem_email").value;
+
+    // Copy the email value to the hidden input
+    document.getElementById("mem_email_hidden").value = emailValue;
+
+    // Disable the email input and button
+    document.getElementById("mem_email").disabled = true;
+    document.getElementById("email_auth").disabled = true;
+}
+
 function checkPasswordMatch() {
     var password = $('#password').val();
     var confirmPassword = $('#mem_pwd_ck').val();
@@ -30,42 +37,42 @@ function checkPasswordMatch() {
 }
 
 $(function () {
-	
-	
-	$('#join').click(function(){
-		console.log(emailOk);
-		
-		if($('#password').val()!=$('#mem_pwd_ck').val()){
-			alert("비밀번호가 일치하지 않습니다.");
-			$("#password").focus();
-			return false;
-		}
-		if($('#mem_email').val()==""){
-			alert("이메일을 입력해주세요.");
-			$('#mem_email').focus();
-			return false;
-		}
-		if($('#password').val()==""){
-			alert("비밀번호를 입력해주세요.");
-			$('#password').focus();
-			return false;
-		}
-		if(emailOk=='false'){
-			alert("이메일 인증을 진행해주세요");
-			return false;
-		}
-	})
+
+
+    $('#join').click(function(){
+        console.log(emailOk);
+
+        if($('#password').val()!=$('#mem_pwd_ck').val()){
+            alert("비밀번호가 일치하지 않습니다.");
+            $("#password").focus();
+            return false;
+        }
+        if($('#mem_email').val()==""){
+            alert("이메일을 입력해주세요.");
+            $('#mem_email').focus();
+            return false;
+        }
+        if($('#password').val()==""){
+            alert("비밀번호를 입력해주세요.");
+            $('#password').focus();
+            return false;
+        }
+        if(emailOk=='false'){
+            alert("이메일 인증을 진행해주세요");
+            return false;
+        }
+    })
 })
 
 
-	
-	$(document).ready(function() {
+
+$(document).ready(function() {
     fn_email();
 });
 
 function fn_email() {
     $('#email_auth').click(function() {
-		
+
         var email = $('#mem_email').val();
 
         if (email=='') {
@@ -83,15 +90,15 @@ function fn_email() {
             success: function(response) {
                 // 성공 시 처리할 코드
                 if(response==false){
-					alert("중복된 이메일입니다.");
-					document.getElementById('mem_email').value = '';
-					return false;   
-				}
+                    alert("중복된 이메일입니다.");
+                    document.getElementById('mem_email').value = '';
+                    return false;
+                }
                 else if (response==true){
-                console.log("데이터넘어감");
-                alert("인증번호가 전송되었습니다.");
-                $("#mail_check").slideDown();
-                fn_emailOk();
+                    console.log("데이터넘어감");
+                    alert("인증번호가 전송되었습니다.");
+                    $("#mail_check").slideDown();
+                    fn_emailOk();
                 }
             },
             error: function(error) {
@@ -101,11 +108,11 @@ function fn_email() {
     });
 }
 
-/*인증버튼클릭시 인증값 넘겨준후 비교 
+/*인증버튼클릭시 인증값 넘겨준후 비교
  값일치하면 true값 가져와서 인증성공진행*/
 function fn_emailOk() {
     $('#mailcheck').click(function() {
-		
+
         var emailnum = $('#number').val();
 
         if (emailnum=='') {
@@ -123,19 +130,19 @@ function fn_emailOk() {
             success: function(response) {
                 // 성공 시 처리할 코드
                 if(response==true){
-					alert("인증이 완료되었습니다.");
-					$("#mail_check").slideUp();
-					emailOk ='true';
-					disableEmailField();
-					return;
-				} else if(response==false){
-					alert("인증번호가 일치하지 않습니다.");
-					$("#number").focus();
-				}
-              return;
-                
-                
-                
+                    alert("인증이 완료되었습니다.");
+                    $("#mail_check").slideUp();
+                    emailOk ='true';
+                    disableEmailField();
+                    return;
+                } else if(response==false){
+                    alert("인증번호가 일치하지 않습니다.");
+                    $("#number").focus();
+                }
+                return;
+
+
+
             },
             error: function(error) {
                 // 에러 시 처리할 코드
@@ -153,7 +160,7 @@ $(document).ready(function() {
 
 function fn_emailpwd() {
     $('#email_auth_pwd').click(function() {
-		
+
         var email = $('#mem_email').val();
 
         if (email=='') {
@@ -171,19 +178,19 @@ function fn_emailpwd() {
             success: function(response) {
                 // 성공 시 처리할 코드
                 if(response==false){
-					alert("존재하지 않는 회원입니다.");
-					document.getElementById('email_auth_pwd').value = '';
-					return false;   
-				}
+                    alert("존재하지 않는 회원입니다.");
+                    document.getElementById('email_auth_pwd').value = '';
+                    return false;
+                }
                 else if (response==true){
-                console.log("데이터넘어감");
-                alert("인증번호가 전송되었습니다.");
-                $("#mail_check").slideDown();
-                fn_emailpwdOk();
+                    console.log("데이터넘어감");
+                    alert("인증번호가 전송되었습니다.");
+                    $("#mail_check").slideDown();
+                    fn_emailpwdOk();
                 }
             },
             error: function(error) {
-               console.log("존재하지 않는 이메일");
+                console.log("존재하지 않는 이메일");
             }
         });
     });
@@ -192,7 +199,7 @@ function fn_emailpwd() {
 
 function fn_emailpwdOk() {
     $('#mailcheck').click(function() {
-		
+
         var emailnum = $('#number').val();
 
         if (emailnum=='') {
@@ -210,17 +217,20 @@ function fn_emailpwdOk() {
             success: function(response) {
                 // 성공 시 처리할 코드
                 if(response==true){
-					alert("인증이 완료되었습니다.");
-					fn_sendpwd();
-					
-				} else if(response==false){
-					alert("인증번호가 일치하지 않습니다.");
-					$("#number").focus();
-				}
-              return;
-                
-                
-                
+                    alert("인증이 완료되었습니다.");
+                    alert("랜덤비밀번호가 발송되었습니다.");
+                    fn_sendpwd();
+
+                    window.location.href="/mugja/login"
+
+                } else if(response==false){
+                    alert("인증번호가 일치하지 않습니다.");
+                    $("#number").focus();
+                }
+                return;
+
+
+
             },
             error: function(error) {
                 // 에러 시 처리할 코드
@@ -231,46 +241,46 @@ function fn_emailpwdOk() {
 
 
 function fn_sendpwd() {
-  
-		var email = $('#mem_email').val();
-		
-        $.ajax({
-            type: "POST",
-            url: "/api/member/emailSendPwd",
-            data: JSON.stringify({ mem_email: email }),
-            contentType : "application/json",
-            datatype : "json",
-            success: function(data) {
-                window.location.href="/mugja/pwdchgemail";
-					return;
-				},
-            error: function(error) {
-               console.log("잘못된 접근");
-            }
-        });
+
+    var email = $('#mem_email').val();
+
+    $.ajax({
+        type: "POST",
+        url: "/api/member/emailSendPwd",
+        data: JSON.stringify({ mem_email: email }),
+        contentType : "application/json",
+        datatype : "json",
+        success: function(data) {
+            window.location.href="/mugja/pwdchgemail";
+            return;
+        },
+        error: function(error) {
+            console.log("잘못된 접근");
+        }
+    });
 }
 
 function fn_pwdchk() {
     $('#pwdchg').click(function() {
-		
+
         var mem_pwd = $('#mem_pwd').val();
-		var password = $('#password').val();
-		var password_ck = $('#mem_pwd_ck').val();
-		
+        var password = $('#password').val();
+        var password_ck = $('#mem_pwd_ck').val();
+
         if (mem_pwd=='') {
             alert("현재 비밀번호를 입력해주세요");
             $('#mem_pwd').focus();
             return false;
         }
         if(password==''){
-			alert("변경하실 비밀번호를 입력해주세요");
-			$('#password').focus();
-			return false;
-		}
-		if(password!=password_ck){
-			alert("새 비밀번호가 일치하지 않습니다.");
-			return false;
-		}
+            alert("변경하실 비밀번호를 입력해주세요");
+            $('#password').focus();
+            return false;
+        }
+        if(password!=password_ck){
+            alert("새 비밀번호가 일치하지 않습니다.");
+            return false;
+        }
 
 
         $.ajax({
@@ -283,20 +293,20 @@ function fn_pwdchk() {
                 // 성공 시 처리할 코드
                 console.log(response);
                 if(response==true){
-					alert("비밀번호 변경이 완료되었습니다.");
-					$('#passwordForm').submit();
-				} else if(response==false){
-					alert("비밀번호 변경에 실패하였습니다.");
-					$("#number").focus();
-					return false;
-				}
-             
-                
-                
-                
+                    alert("비밀번호 변경이 완료되었습니다.");
+                    $('#passwordForm').submit();
+                } else if(response==false){
+                    alert("비밀번호 변경에 실패하였습니다.");
+                    $("#number").focus();
+                    return false;
+                }
+
+
+
+
             },
             error: function(error) {
-               alert("에러발생");
+                alert("에러발생");
             }
         });
     });
