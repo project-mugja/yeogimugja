2dc4a0b98db98a3af73898a32dbf2fe6df259644
ljh0808 <zmdk200632@gmail.com>
2024-07-18 15:10:50 +0900

    dasdsa

--- src/main/java/com/mugja/member/domain/MemberRepository.java
+++ src/main/java/com/mugja/member/domain/MemberRepository.java
@@ -2,6 +2,6 @@ package com.mugja.member.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Integer> {
 
 }
